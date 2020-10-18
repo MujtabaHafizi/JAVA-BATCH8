2cdeccfad7bc059b9426e14bbb02b169a4cd07ec
MujtabaHafizi <muj.bdn@gmail.com>
2020-10-17 23:24:04 -0400

    Still creating while loops

--- src/Class7Oct17/Class2Loops.java
+++ src/Class7Oct17/Class2Loops.java
@@ -6,34 +6,31 @@ public class Class2Loops {
 
 		int time = 15;
 		if(time>12) {
-			System.out.println("Good day");
+		System.out.println("Good day"); // Good Day is printed here casue 12 is smaller than 15.
 		}
 		
 		System.out.println("\n");
-
 		while (time>12) {
-			System.out.println("Good day");
-			time--;
+		System.out.println("Good day"); // Good Day is printed 3 times
+		time--;
 		}
 		
-		
 		System.out.println("\n");
-
-		while (time>12) {
-			System.out.println("Good day");
-			time++;
+		while (time>2) {
+		System.out.println(time); // it never stops printing Good Day
+		time--;
 	}
 		// i want to print number from 1 to 15
-		System.out.println("Printing number 1  to 50");
+		System.out.println("------------------Printing number 1  to 50-------------------\n");
 
 		int num = 0;
 		while(num<=15) {
-			System.out.println(num);
-			num++;
+		System.out.println(num);
+		num++;
 		}
 			
 		// i want to print number from 10 to 60
-				System.out.println("Printing number 1  to 50");
+		System.out.println("----------------------------Printing number 1  to 50-----------------------\n");
 		int  num1 = 10;
 		while(num1<=60) {
 			System.out.println(num1+" ");
